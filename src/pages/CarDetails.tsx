274213a5f4ea51e77d3998101cf6d9660909d9e3
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-09-28 22:18:03 +0300

    fix

--- src/pages/CarDetails.tsx
+++ src/pages/CarDetails.tsx
@@ -112,7 +112,7 @@ const CarDetails = () => {
   return (
     <Container>
       <Toaster position="top-right" />
-      <div className="flex-col-reverse max-w-[1400px] mx-auto flex flex-col lg:flex-row lg:justify-center gap-8 md:gap-12 lg:gap-[72px] pt-10 md:pt-16 lg:pt-[84px] pb-16 md:pb-20 lg:pb-[104px]">
+      <div className="flex-col-reverse max-w-[1400px] mx-auto flex flex-col lg:flex-row lg:justify-center gap-[32px] md:gap-[48px] lg:gap-[72px] pt-[40] md:pt-[64px] lg:pt-[84px] pb-[64px] md:pb-[80px] lg:pb-[104px]">
         {/* img + form container */}
         <div className="flex flex-col gap-[40px] w-[640px]">
           <img
